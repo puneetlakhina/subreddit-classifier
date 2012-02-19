b614356ca3c88b825e9dd441eafcbc4c424d6bf7
Puneet Lakhina <puneet.lakhina@gmail.com>
2012-02-18 19:47:55 -0800

    Add nbsp stop word. Make stop word processing case insensitive

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -25,15 +25,15 @@ class NaiveBayesClassifier:
         words = utils.read_cached(self.cache_dir + "/" + docid ,self.preprocessor.process, content)
         if not words:
             return
-        #Now make sure we havent already used this example for this label
         self.labels_metadata.setdefault(label, {"doc_ids":[] , "word_doc_counts": {} })
+        #Now make sure we havent already used this example for this label
         label_metadata = self.labels_metadata[label]
         if docid in label_metadata["doc_ids"]:
             #We have already processed this url
             return;
         else:
             label_metadata["doc_ids"].append(docid)
-            words = set(words)
+            words = set(words) #deduplication of words.
             for word in words:
                 label_metadata["word_doc_counts"].setdefault(word,1)
                 label_metadata["word_doc_counts"][word] = label_metadata["word_doc_counts"][word] + 1
@@ -47,17 +47,22 @@ class NaiveBayesClassifier:
             for word in words:
                 tot_doc_count = len(label_data["doc_ids"])
                 if word in label_data["word_doc_counts"]:
-                    prob_sum = prob_sum + math.log(float(label_data["word_doc_counts"][word]))/tot_doc_count
+                    prob = math.log(float(label_data["word_doc_counts"][word])/tot_doc_count)
+                    logger.debug("Found word: %s. Prob %f. Count in the data: %d" % (word,prob, label_data["word_doc_counts"][word]))
+                    prob_sum = prob_sum + prob
             probs[label] = prob_sum
             if not argmax["prob"] or prob_sum > argmax["prob"]:
                 argmax["prob"] = prob_sum
                 argmax["label"] = label
         logger.debug("URl: %s, debuginfo: %s " % (url,str(probs)))
         return argmax
+    def debug_info(self, label):
+        return self.labels_metadata[label]
 help_str=sys.argv[0] + """
 -t training_set_size
 -c cross_validation_set_size
 -s subreddits(comma separated).May be specified multiple times
+-d Print debug info about the subreddits classification data
 -h help
 """
 def usage(extra_str=""):
@@ -69,8 +74,9 @@ if __name__ == "__main__":
     ts = 0
     cv = 0
     subreddits=[]
+    debug_info = False
     try:
-        opts,args = getopt.getopt(sys.argv[1:],"t:c:s:hv")
+        opts,args = getopt.getopt(sys.argv[1:],"t:c:s:dhv")
         for opt,val in opts:
             if opt == "-t":
                 ts = int(val)
@@ -80,8 +86,10 @@ if __name__ == "__main__":
                 subreddits.extend(map(lambda x:x.strip(), val.split(",")))
             elif opt == "-h":
                 usage()
-        if ts == 0 and cv == 0:
-            usage("One of training/cross validation set size must be specified")
+            elif opt == "-d":
+                debug_info = True
+        if ts == 0 and cv == 0 and not debug_info:
+            usage("One of training/cross validation set size must be specified. Or debug info flag must be enabled")
         if len(subreddits) == 0:
             usage("No subreddits specified\n")
         classifier = NaiveBayesClassifier()
@@ -94,6 +102,9 @@ if __name__ == "__main__":
                 for url,content in subred_manager.get_subred_content(content_fetcher=content_fetcher,nstories=ts):
                     logging.debug("URL:%s" % str(url)) 
                     classifier.train(url, content, subreddit)
+        if debug_info:
+            for subreddit in subreddits:
+                print classifier.debug_info(subreddit)
         if cv > 0:
             logger.debug("Cross validation");
             for subreddit in subreddits:
